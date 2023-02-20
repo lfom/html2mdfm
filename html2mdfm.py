e23f040570aeea786bea5d00f16369614765cbda
lfom <lfom-dev@gmx.com>
2023-02-20 14:21:15 -0300

    New code
    Updated code

--- html2mdfm.py
+++ html2mdfm.py
@@ -1,45 +1,169 @@
-import json
-import os
-import bs4
+#!/usr/bin/env python3
+import os, json, bs4, re, argparse, shutil
 from bs4 import BeautifulSoup
 from rich.console import Console
 from rich.theme import Theme
-from markdownify import markdownify
+from markdownify import markdownify as md
+from pathlib import Path
+from datetime import datetime, timezone
 
-custom_theme = Theme({
-    "line_style": "bright_yellow b",
-    "warn": "bright_yellow",
-    "att": "bright_red",
-    "val": "bright_magenta",
-    "sep": "bright_cyan",
-    "var": "yellow",
-})
-console = Console(theme=custom_theme)
+SAVE_FOLDER = "export_data"
 
+def set_console():
+    custom_theme = Theme({
+        "line_style": "bright_yellow b",
+        "warn": "bright_yellow",
+        "att": "bright_red",
+        "val": "bright_magenta",
+        "sep": "bright_cyan",
+        "var": "yellow",
+    })
+    return Console(theme=custom_theme)
 
-def get_current_time() -> str:
+def get_time() -> str:
     today = console.get_datetime()
-
     hour = str(today.hour)
     minute = str(today.minute)
     second = str(today.second)
-
     if len(hour) == 1:
         hour = "0" + hour
     if len(minute) == 1:
         minute = "0" + minute
     if len(second) == 1:
         second = "0" + second
-
     return f'[b][[bright_green]{hour}:{minute}:{second}[/bright_green]][/b]'
 
+def format_time(value) -> str:
+    pattern = r'^(\+|\-)\d{4}$'
+    tz = value[-5:]
+    date = value
+    if bool(re.match(pattern, tz)):
+        date = value[:-2] + ":" + value[-2:]
+    date_tz = datetime.fromisoformat(date)
+    date = str(date_tz.astimezone(timezone.utc).isoformat())
+    time = date.split("T")
+    formatted_date = time[0]
+    formatted_time = time[1].split('+')[0]
+    return formatted_date + " " + formatted_time + "Z"
 
-export_path = console.input(f"{get_current_time()} Enter the path to the export folder\n")
-is_export_path_correct = os.path.exists(export_path)
-if is_export_path_correct:
+def replace_illegal(value, full = True) -> str:
+    replaced = value
+    if full:
+        illegal_chars = '\/:*?"<>|'
+        replacement = ' '
+    else:
+        illegal_chars = ':<>|'
+        replacement = ''
+
+    for c in illegal_chars:
+        replaced = replaced.replace(c, replacement)
+    return replaced
+
+def filter_content(value, line_breaks):
+    filters = ['<input type="checkbox"/><span>', '<input checked="true" type="checkbox"/><span>']
+    checkbox_start = ['- [ ] ', '- [x] ']
+    end = '</span>'
+    filtered = value
+    is_todo = False
+    for c, start in enumerate(filters):
+        start_index = filtered.find(start)
+        while start_index > -1:
+            end_index = filtered.find(end)
+            if end_index > -1:
+                end_index += 7
+                subs = filtered[start_index : end_index]
+                checkbox_item = subs.replace(start, checkbox_start[c])
+                checkbox_item = checkbox_item.replace(end, '')
+                filtered = filtered.replace(subs, checkbox_item)
+                start_index = filtered.find(start)
+                is_todo = True
+            else:
+                start_index = -1
+    if line_breaks:
+        filtered = filtered.replace('</div><div>', '</div><br/></div>')
+    return (is_todo, filtered)
+
+def find_attachments(value):
+    starts = ['<img src="', '<a href="']
+    ends = ['"/>', '">']
+    attachments = []
+    for c, start in enumerate(starts):
+        subs = value
+        start_index = subs.find(start)
+        while start_index > -1:
+            if start_index > 1:
+                remove = subs[0 : start_index]
+                subs = subs.replace(remove, '')
+            else:
+                subs = subs[1:]
+            end_index = subs.find(ends[c])
+            if end_index > -1:
+                attachment_start = len(start)
+                attachment = subs[attachment_start : end_index]
+                end_index += len(ends[c])
+                remove = subs[0 : end_index]
+                attachments.append(attachment)
+                subs = subs.replace(remove, '')
+                start_index = subs.find(start)
+            else:
+                start_index = -1
+
+    if value.find('style="-evernote-webclip:true"') > -1:
+        attachments.append('**evernote-webclip**')
+    return attachments
+
+def move_attachments(attachments, source_dir, destination_dir):
+    if attachments:
+        for attachment in attachments:
+            for f_name in os.listdir(source_dir):
+                if attachment == str(f_name):
+                    src = source_dir / attachment
+                    dst = destination_dir / attachment
+                    try:
+                        shutil.copy(src, dst)
+                    except:
+                        console.print(f'{get_time()} [att][b]ERROR:[/b] Could not copy {attachment}')
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='Convert Zoho Notebook HTML export to (Markdown + Front Matter) that can be imported to Joplin')
+
+# Required argument
+parser.add_argument('infolder', type=str, help='input folder that contains the exported HTML files')
+
+# Optional positional argument
+parser.add_argument('outfolder', type=str, nargs='?', help='output folder [if not provided, the current folder will be used]')
+
+# Switch
+parser.add_argument('--nolinebreaks', action='store_false', help='disable adding line breaks between divs (text lines may get merged into blocks)')
+
+# Version
+parser.add_argument('--version', action='version', version='1.0')
+
+args = parser.parse_args()
+
+export_path = Path(os.path.abspath(args.infolder))
+
+if args.outfolder:
+    save_path = Path(os.path.abspath(args.outfolder))
+else:
+    save_path = Path(os.path.abspath(os.getcwd()))
+tmp_path = save_path / SAVE_FOLDER
+
+line_breaks = args.nolinebreaks
+
+console = set_console()
+
+console.print(f'{get_time()} HTML files from {export_path}')
+console.print(f'{get_time()} will be exported to {tmp_path}')
+console.print(f'{get_time()} Line breaks between <div>\'s: {line_breaks}')
+console.print(f'{get_time()} Do you want to continue (y/N)?')
+if not input().lower().strip()[:1] == "y": exit()
+
+if os.path.exists(export_path):
     filenames = []
     try:
-        with open(f"{export_path}/index.html") as fp:
+        index_file = export_path / "index.html"
+        with open(index_file) as fp:
             soup = BeautifulSoup(fp, "html.parser")
 
             for li in soup.find_all("li"):
@@ -47,85 +171,103 @@ if is_export_path_correct:
                 filenames.append(li.find("a").get("href"))
 
     except NotADirectoryError:
-        console.print(f'{get_current_time()} [att][b]ERROR:[/b] The path must point to a folder, not a file')
+        console.print(f'{get_time()} [att][b]ERROR:[/b] The path must point to a folder, not a file')
         exit()
     except FileNotFoundError:
-        console.print(f'{get_current_time()} [att][b]ERROR:[/b] Folder does not contain index.html file')
+        console.print(f'{get_time()} [att][b]ERROR:[/b] Folder does not contain index.html file')
         exit()
     filedatas = []
     for index, filename in enumerate(filenames):
         try:
-            with open(f"{export_path}/{filename}") as fp:
+            open_file = export_path / filename
+            with open(open_file) as fp:
                 soup = BeautifulSoup(fp, "html.parser")
-                filedatas.append(
-                    {"title": soup.find("title").text})
+                filedatas.append({"title": soup.find("title").text})
 
-                dirty_data: dict[str, str] = json.loads(soup.find("body").get("data-notebook"))
+                json_data = soup.find("body").get("data-notebook")
+                dirty_data: dict[str, str] = json.loads(json_data)
 
-                dirty_full_created_date = dirty_data["created_date"].split("T")
-                created_date = dirty_full_created_date[0]
-                created_time = dirty_full_created_date[1].split("-")[0]
-                filedatas[index]["created"] = created_date + " " + created_time + "Z"
-
-                dirty_full_updated_date = dirty_data["modified_date"].split("T")
-                updated_date = dirty_full_updated_date[0]
-                updated_time = dirty_full_updated_date[1].split("-")[0]
-                filedatas[index]["updated"] = updated_date + " " + updated_time + "Z"
-
-                filedatas[index]["completed?"] = "no"
                 filedatas[index]["folder_name"] = dirty_data["name"]
 
-                today = console.get_datetime()
-                year = str(today.year)
-                match = str(today.month)
-                day = str(today.day)
-                hour = str(today.hour)
-                minute = str(today.minute)
-                second = str(today.second)
-                if len(match) == 1:
-                    match = "0" + match
-                if len(day) == 1:
-                    day = "0" + day
-                if len(hour) == 1:
-                    hour = "0" + hour
-                if len(minute) == 1:
-                    minute = "0" + minute
-                if len(second) == 1:
-                    second = "0" + second
-                filedatas[index]["due"] = f"{year}-{match}-{day} {hour}:{minute}:{second}Z"
+                filedatas[index]["created"] = format_time(dirty_data["created_date"])
 
-                dirty_content = str(soup.html)
-                filedatas[index]["content"] = markdownify(dirty_content)
+                filedatas[index]["updated"] = format_time(dirty_data["modified_date"])
+
+                dirty_content = str(soup.body)
+                is_todo, filtered_content = filter_content(dirty_content, line_breaks)
+
+                filedatas[index]["is_todo"] = is_todo
+                if is_todo:
+                    filedatas[index]["completed?"] = "no"
+                    filedatas[index]["due"] = ''
+
+                json_data = soup.find("body").get("data-remainder")
+                if json_data and len(json_data) > 1:
+                    json_data = json_data[1 : len(json_data)-1]
+                    dirty_rem: dict[str, str] = json.loads(json_data)
+                    if dirty_rem["type"].find("reminder") > -1:
+                        filedatas[index]["is_todo"] = True
+                        if dirty_rem["is-completed"]:
+                            filedatas[index]["completed?"] = "yes" if dirty_rem["is-completed"] == 1 else "no"
+                        if dirty_rem["ZReminderTime"]:
+                            filedatas[index]["due"] = format_time(dirty_rem["ZReminderTime"])
+
+                attachments = find_attachments(filtered_content)
+                filedatas[index]["attachments"] = attachments
+
+                filedatas[index]["content"] = md(
+                    filtered_content, heading_style="ATX", keep_inline_images_in=['td']
+                    )
+
+                attachment_list = ''
+                if attachments:
+                    for attachment in attachments:
+                        if attachment.startswith('http') or attachment.find('webclip') > -1:
+                            attachment_list += attachment + '  \n'
+                if attachment_list:
+                    filedatas[index]["content"] += f'  \n{attachment_list}'
 
         except NotADirectoryError:
-            console.print(f'{get_current_time()} [att][b]ERROR:[/b] The path does not exist')
+            console.print(f'{get_time()} [att][b]ERROR:[/b] The path does not exist')
         except FileNotFoundError:
-            console.print(f'{get_current_time()} [att][b]ERROR:[/b] The folder does not contain {filename}')
-    save_path = console.input(f"{get_current_time()} Enter the path where to save the data\n")
-    is_save_path_correct = os.path.exists(save_path)
-    if is_save_path_correct:
+            console.print(f'{get_time()} [att][b]ERROR:[/b] File {filename} not found')
+
+    if os.path.exists(save_path):
         try:
-            os.mkdir(f"{save_path}/export_data")
+            tmp_path.mkdir(exist_ok=True)
         except:
-            pass
+            console.print(f'{get_time()} [att][b]ERROR:[/b] Could not create {tmp_path}')
+            exit()
         for index, filedata in enumerate(filedatas):
+            save_folder = tmp_path / replace_illegal(filedata["folder_name"])
             try:
-                os.mkdir(f'{save_path}/export_data/{filedata["folder_name"]}')
+                save_folder.mkdir(exist_ok=True)
             except:
-                pass
+                console.print(f'{get_time()} [att][b]ERROR:[/b] Could not create {save_folder}')
+                exit()
+
             content = [f'---\n',
-                       f'title: {filedata["title"]}\n',
+                       f'title: {replace_illegal(filedata["title"], False)}\n',
                        f'created: {filedata["created"]}\n',
-                       f'updated: {filedata["updated"]}\n',
-                       f'completed?: {filedata["completed?"]}\n',
-                       f'due: {filedata["due"]}\n',
-                       f'---\n',
+                       f'updated: {filedata["updated"]}\n']
+
+            if filedata["is_todo"]:
+                content += [f'completed?: {filedata["completed?"]}\n']
+                if filedata["due"]:
+                       content += [f'due: {filedata["due"]}\n']
+
+            content += [f'---\n',
                        f'\n',
                        f'{filedata["content"]}\n']
 
-            with open(f'{save_path}/export_data/{filedata["folder_name"]}/{filedata["title"]} {index}.md', "w") as f:
+            file_name = f'{replace_illegal(filedata["title"])} {index}.md'
+            save_file = save_folder / file_name
+            with open(save_file, "w") as f:
                 f.writelines(content)
+
+            move_attachments(filedata["attachments"], export_path, save_folder)
+
     else:
-        console.print(f'{get_current_time()} [att][b]ERROR:[/b] The path does not exist')
+        console.print(f'{get_time()} [att][b]ERROR:[/b] The path does not exist')
 else:
-    console.print(f'{get_current_time()} [att][b]ERROR:[/b] The path does not exist')
+    console.print(f'{get_time()} [att][b]ERROR:[/b] The path does not exist')
